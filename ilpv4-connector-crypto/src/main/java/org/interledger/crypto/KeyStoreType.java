5210c676dcc83afd7c48163c5b7bab7c2bff2a48
David Fuelling <sappenin@gmail.com>
2019-05-04 16:52:09 -0600

    Misc cleanup
    Signed-off-by: David Fuelling <sappenin@gmail.com>

--- ilpv4-connector-crypto/src/main/java/org/interledger/crypto/KeyStoreType.java
+++ ilpv4-connector-crypto/src/main/java/org/interledger/crypto/KeyStoreType.java
@@ -5,11 +5,11 @@ import static org.interledger.crypto.CryptoConstants.KEYSTORE_ID_JKS;
 import static org.interledger.crypto.CryptoConstants.KEYSTORE_ID_VAULT;
 
 /**
- * Unique identifiers of the providers of underlying secrets-storage, which typically treat key-material as "secret
- * information" whereas the data encrypted by these secrets is generally not stored in these platforms. For example, a
- * database password would not be stored in the secrets-store. Instead, a private key or symmetric key would be stored
- * in the secrets store, and the encrypted database password (which is also _technically_ a secret value) would be
- * stored in some other system.
+ * <p>Defines unique identifiers for the providers of underlying key-storage, which typically treat key-material as
+ * "secret information" whereas the data encrypted by these keys is generally not stored in these platforms.</p>
+ *
+ * <p>For example, a database password would not be stored in the key-store. Instead, a private key or symmetric
+ * key would be stored in the key-store, and the encrypted database password would be stored in some other system.</p>
  */
 public enum KeyStoreType {
 
