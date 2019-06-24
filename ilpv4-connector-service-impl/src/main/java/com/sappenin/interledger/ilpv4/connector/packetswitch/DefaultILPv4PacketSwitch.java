db2888ffd963af87e29d0292407ecfc43ce257e6
David Fuelling <sappenin@gmail.com>
2019-06-24 17:22:57 -0600

    Fixes #142
    * Enable cached AccountSettings to improve performance.
    * Introduce Caffeine (https://github.com/ben-manes/caffeine), a high-performance Java cache, into the codebase, because it offers much better perf than Guava cache.
    
    Signed-off-by: David Fuelling <sappenin@gmail.com>

--- ilpv4-connector-service-impl/src/main/java/com/sappenin/interledger/ilpv4/connector/packetswitch/DefaultILPv4PacketSwitch.java
+++ ilpv4-connector-service-impl/src/main/java/com/sappenin/interledger/ilpv4/connector/packetswitch/DefaultILPv4PacketSwitch.java
@@ -1,5 +1,7 @@
 package com.sappenin.interledger.ilpv4.connector.packetswitch;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.sappenin.interledger.ilpv4.connector.ConnectorExceptionHandler;
 import com.sappenin.interledger.ilpv4.connector.links.LinkManager;
 import com.sappenin.interledger.ilpv4.connector.links.NextHopPacketMapper;
@@ -12,10 +14,12 @@ import org.interledger.core.InterledgerErrorCode;
 import org.interledger.core.InterledgerPreparePacket;
 import org.interledger.core.InterledgerProtocolException;
 import org.interledger.core.InterledgerResponsePacket;
+import org.interledger.ilpv4.connector.persistence.entities.AccountSettingsEntity;
 import org.interledger.ilpv4.connector.persistence.repositories.AccountSettingsRepository;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A default implementation of {@link ILPv4PacketSwitch}.
@@ -30,6 +34,13 @@ public class DefaultILPv4PacketSwitch implements ILPv4PacketSwitch {
   private final AccountSettingsRepository accountSettingsRepository;
   private final PacketRejector packetRejector;
 
+  // Loading from the Database is somewhat expensive, so we don't want to do this on every packet processed for a
+  // given account. Instead, for higher performance, we only load account settings once per period, and otherwise
+  // rely upon AccountSettings found in this cache. This design is preferable to using Hibernate's 2nd-level cache
+  // because in-general, we don't want to Cache accountSettings to support clustered Connector environments (though
+  // this may change in the future depending on benchmark results).
+  private final LoadingCache<AccountId, AccountSettingsEntity> accountSettingsCache;
+
   public DefaultILPv4PacketSwitch(
     final List<PacketSwitchFilter> packetSwitchFilters,
     final List<LinkFilter> linkFilters,
@@ -39,6 +50,29 @@ public class DefaultILPv4PacketSwitch implements ILPv4PacketSwitch {
     final AccountSettingsRepository accountSettingsRepository,
     final PacketRejector packetRejector
   ) {
+    this(packetSwitchFilters, linkFilters, linkManager, nextHopPacketMapper, connectorExceptionHandler,
+      accountSettingsRepository, packetRejector,
+      Caffeine.newBuilder()
+        .expireAfterAccess(15, TimeUnit.MINUTES) // TODO Make this duration configurable
+        .refreshAfterWrite(20, TimeUnit.SECONDS) // TODO Make this duration configurable
+        .maximumSize(5000) // TODO: Make size configurable.
+        .build(sourceAccountId -> accountSettingsRepository.findByAccountId(sourceAccountId).orElse(null))
+    );
+  }
+
+  /**
+   * For testing purposes.
+   */
+  DefaultILPv4PacketSwitch(
+    final List<PacketSwitchFilter> packetSwitchFilters,
+    final List<LinkFilter> linkFilters,
+    final LinkManager linkManager,
+    final NextHopPacketMapper nextHopPacketMapper,
+    final ConnectorExceptionHandler connectorExceptionHandler,
+    final AccountSettingsRepository accountSettingsRepository,
+    final PacketRejector packetRejector,
+    final LoadingCache<AccountId, AccountSettingsEntity> accountSettingsCache
+  ) {
     this.packetSwitchFilters = Objects.requireNonNull(packetSwitchFilters);
     this.linkFilters = Objects.requireNonNull(linkFilters);
     this.linkManager = Objects.requireNonNull(linkManager);
@@ -46,6 +80,7 @@ public class DefaultILPv4PacketSwitch implements ILPv4PacketSwitch {
     this.connectorExceptionHandler = Objects.requireNonNull(connectorExceptionHandler);
     this.accountSettingsRepository = Objects.requireNonNull(accountSettingsRepository);
     this.packetRejector = Objects.requireNonNull(packetRejector);
+    this.accountSettingsCache = Objects.requireNonNull(accountSettingsCache);
   }
 
   public final InterledgerResponsePacket switchPacket(
@@ -54,21 +89,22 @@ public class DefaultILPv4PacketSwitch implements ILPv4PacketSwitch {
     Objects.requireNonNull(sourceAccountId);
     Objects.requireNonNull(incomingSourcePreparePacket);
 
-    // TODO: Ensure this is cached!
-    final AccountSettings sourceAccountSettings = accountSettingsRepository.findByAccountId(sourceAccountId)
+    final AccountSettings sourceAccountSettings = this.accountSettingsCache.get(sourceAccountId);
+    if (sourceAccountSettings == null) {
       // REJECT due to no account...
-      .orElseThrow(() -> new InterledgerProtocolException(
+      throw new InterledgerProtocolException(
         packetRejector.reject(sourceAccountId, incomingSourcePreparePacket, InterledgerErrorCode.T00_INTERNAL_ERROR,
-          String.format("No Account found: `%s`", sourceAccountId))));
+          String.format("No Account found: `%s`", sourceAccountId)));
+    }
 
     try {
-
       return new DefaultPacketSwitchFilterChain(packetSwitchFilters, linkFilters, linkManager, nextHopPacketMapper)
         .doFilter(sourceAccountSettings, incomingSourcePreparePacket);
 
     } catch (Exception e) {
       // Any rejections should be caught here, and returned as such....
       return this.connectorExceptionHandler.handleException(sourceAccountId, incomingSourcePreparePacket, e);
+
     }
   }
 }
