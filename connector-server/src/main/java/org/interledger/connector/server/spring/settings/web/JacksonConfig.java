96a93feb1887cd675bc3750e1ff3db37784ec152
nhartner <nhartner@gmail.com>
2019-10-16 10:22:50 -0600

    move FAIL_ON_UNKNOWN_PROPERTIES to ObjectMapperFactory

--- connector-server/src/main/java/org/interledger/connector/server/spring/settings/web/JacksonConfig.java
+++ connector-server/src/main/java/org/interledger/connector/server/spring/settings/web/JacksonConfig.java
@@ -2,14 +2,7 @@ package org.interledger.connector.server.spring.settings.web;
 
 import org.interledger.connector.jackson.ObjectMapperFactory;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.guava.GuavaModule;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -20,15 +13,7 @@ public class JacksonConfig {
   @Bean
   @Primary
   public ObjectMapper objectMapper() {
-    return ObjectMapperFactory.create()
-        .registerModule(new Jdk8Module())
-        .registerModule(new JavaTimeModule())
-        .registerModule(new GuavaModule())
-        .configure(JsonGenerator.Feature.WRITE_NUMBERS_AS_STRINGS, true)
-        .configure(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN, true)
-        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-        .setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+    return ObjectMapperFactory.create();
   }
 
 }
