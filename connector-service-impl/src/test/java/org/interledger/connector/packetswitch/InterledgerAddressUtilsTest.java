e53fc4707e2dab98ce6c7732626eef9181bfe0cb
David Fuelling <sappenin@gmail.com>
2019-10-25 16:32:37 -0600

    Add unit test
    Signed-off-by: David Fuelling <sappenin@gmail.com>

--- connector-service-impl/src/test/java/org/interledger/connector/packetswitch/InterledgerAddressUtilsTest.java
+++ connector-service-impl/src/test/java/org/interledger/connector/packetswitch/InterledgerAddressUtilsTest.java
@@ -1,8 +1,10 @@
 package org.interledger.connector.packetswitch;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+import org.interledger.connector.accounts.AccountId;
 import org.interledger.connector.persistence.repositories.AccountSettingsRepository;
 import org.interledger.connector.settings.ConnectorSettings;
 import org.interledger.core.InterledgerAddress;
@@ -14,17 +16,20 @@ import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+
 /**
  * Unit tests for {@link InterledgerAddressUtils}.
  */
 public class InterledgerAddressUtilsTest {
+
+  private static final AccountId ACCOUNT_ID = AccountId.of("foo-account");
 
   @Rule
   public ExpectedException expectedException = ExpectedException.none();
 
   @Mock
   private ConnectorSettings connectorSettingsMock;
-
   @Mock
   private AccountSettingsRepository accountSettingsRepositoryMock;
 
@@ -88,5 +93,83 @@ public class InterledgerAddressUtilsTest {
 
   @Test
   public void isDestinationAllowedFromAccount() {
+    // isPaymentNetworkAddress == true
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID,
+      InterledgerAddress.of("g.foo.receiver"))
+    ).isTrue();
+
+    // ping == true
+    when(connectorSettingsMock.operatorAddress()).thenReturn(InterledgerAddress.of("g.foo"));
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID,
+      InterledgerAddress.of("g.foo.receiver"))
+    ).isTrue();
+
+    // private (internal account)
+    mockAccountAsInternal(true);
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID,
+      InterledgerAddress.of("private.foo.receiver"))
+    ).isTrue();
+
+    // private (not internal account)
+    mockAccountAsInternal(false);
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID,
+      InterledgerAddress.of("private.foo.receiver"))
+    ).isFalse();
+
+    // peer (internal account)
+    mockAccountAsInternal(true);
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID,
+      InterledgerAddress.of("peer.foo.receiver"))
+    ).isFalse();
+
+    // peer (not internal account)
+    mockAccountAsInternal(false);
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID,
+      InterledgerAddress.of("peer.foo.receiver"))
+    ).isTrue();
+
+    // self (only internal allowed to forward)
+    mockAccountAsInternal(true);
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID,
+      InterledgerAddress.of("self.foo.receiver"))
+    ).isTrue();
+
+    // self (not internal account)
+    mockAccountAsInternal(false);
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID,
+      InterledgerAddress.of("self.foo.receiver"))
+    ).isFalse();
+
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID, InterledgerAddress.of("example.foo.receiver"))
+    ).isFalse();
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID, InterledgerAddress.of("local.foo.receiver"))
+    ).isFalse();
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID, InterledgerAddress.of("test.foo.receiver"))
+    ).isTrue();
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID, InterledgerAddress.of("test1.foo.receiver"))
+    ).isTrue();
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID, InterledgerAddress.of("test2.foo.receiver"))
+    ).isTrue();
+    assertThat(interledgerAddressUtils.isDestinationAllowedFromAccount(
+      ACCOUNT_ID, InterledgerAddress.of("test3.foo.receiver"))
+    ).isTrue();
+  }
+
+  private void mockAccountAsInternal(final boolean internal) {
+    when(accountSettingsRepositoryMock.isInternal(ACCOUNT_ID)).thenReturn(Optional.of(internal));
+    when(accountSettingsRepositoryMock.isNotInternal(ACCOUNT_ID)).thenReturn(Optional.of(!internal));
   }
 }
