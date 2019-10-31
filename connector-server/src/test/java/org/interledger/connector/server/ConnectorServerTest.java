a68c0ded63987167e645b810944a028bab8fec0b
nhartner <nhartner@gmail.com>
2019-10-30 23:28:15 -0600

    fix ConnectorServerTest so that schema gets created
    Signed-off-by: nhartner <nhartner@gmail.com>

--- connector-server/src/test/java/org/interledger/connector/server/ConnectorServerTest.java
+++ connector-server/src/test/java/org/interledger/connector/server/ConnectorServerTest.java
@@ -2,17 +2,39 @@ package org.interledger.connector.server;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ConnectorServerTest {
 
+  public static final String SPRING_PROFILES_ACTIVE = "--spring.profiles.active";
+  public static final String SERVER_PORT = "--server.port";
+  private ConnectorServer connectorServer;
+
+  @Before
+  public void setUp() {
+    connectorServer = new ConnectorServer();
+  }
+
+  @After
+  public void tearDown() {
+    connectorServer.stop();
+  }
+
   @Test
   public void migrateOnlyCallsStop() {
-    System.setProperty("--spring.profiles.active", "test,migrate-only");
-    System.setProperty("--server.port", "0");
-    System.setProperty("--spring.liquibase.enabled", "false");
-    ConnectorServer connectorServer = new ConnectorServer();
+    System.setProperty(SPRING_PROFILES_ACTIVE, "test,migrate-only");
+    System.setProperty(SERVER_PORT, "0");
     connectorServer.start();
     assertThat(connectorServer.getContext().isActive()).isFalse();
+  }
+
+  @Test
+  public void serverActiveAfterStart() {
+    System.setProperty(SPRING_PROFILES_ACTIVE, "test");
+    System.setProperty(SERVER_PORT, "0");
+    connectorServer.start();
+    assertThat(connectorServer.getContext().isActive()).isTrue();
   }
 }
