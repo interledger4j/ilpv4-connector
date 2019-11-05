e795d0edf2fb361cbf72cbfd83d299225ea92009
David Fuelling <sappenin@gmail.com>
2019-11-05 16:07:03 -0700

    Add test
    Signed-off-by: David Fuelling <sappenin@gmail.com>

--- connector-service-impl/src/test/java/org/interledger/connector/links/filters/OutgoingStatsLinkFilterTest.java
+++ connector-service-impl/src/test/java/org/interledger/connector/links/filters/OutgoingStatsLinkFilterTest.java
@@ -1,5 +1,13 @@
-import static org.junit.Assert.*;
+package org.interledger.connector.links.filters;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
 
 public class OutgoingStatsLinkFilterTest {
 
+  @Test
+  public void doFilter() {
+    fail();
+  }
 }
