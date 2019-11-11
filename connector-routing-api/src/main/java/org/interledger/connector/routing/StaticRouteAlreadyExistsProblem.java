7cf2d761f8130092008f2fa633ab2fce0eba9f01
theotherian <ian.g.simpson@gmail.com>
2019-11-11 11:24:37 -0800

    Update connector-routing-api/src/main/java/org/interledger/connector/routing/StaticRouteAlreadyExistsProblem.java
    Co-Authored-By: David Fuelling <sappenin@gmail.com>

--- connector-routing-api/src/main/java/org/interledger/connector/routing/StaticRouteAlreadyExistsProblem.java
+++ connector-routing-api/src/main/java/org/interledger/connector/routing/StaticRouteAlreadyExistsProblem.java
@@ -12,7 +12,7 @@ public class StaticRouteAlreadyExistsProblem extends StaticRouteProblem {
   public StaticRouteAlreadyExistsProblem(InterledgerAddressPrefix prefix) {
     super(
         URI.create(TYPE_PREFIX + STATIC_ROUTES_PATH + "/static-route-already-exists"),
-        "Route Already Exists (`" + prefix.getValue() + "`)",
+        "Static Route Already Exists (`" + prefix.getValue() + "`)",
         Status.CONFLICT,
         Objects.requireNonNull(prefix)
     );
