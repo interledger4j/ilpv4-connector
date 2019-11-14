003618e8d27d6c46aa18929f048e6fa30cca3f31
David Fuelling <sappenin@gmail.com>
2019-11-14 14:48:41 -0700

    Remove hamcrest from accounts
    Signed-off-by: David Fuelling <sappenin@gmail.com>

--- connector-accounts/src/test/java/org/interledger/connector/accounts/AccountRelationshipTest.java
+++ connector-accounts/src/test/java/org/interledger/connector/accounts/AccountRelationshipTest.java
@@ -1,10 +1,9 @@
 package org.interledger.connector.accounts;
 
-import org.junit.Test;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
+
+import org.junit.Test;
 
 /**
  * Unit tests for {@link AccountRelationship}.
@@ -17,7 +16,7 @@ public class AccountRelationshipTest {
       AccountRelationship.fromWeight(-1);
       fail();
     } catch (IllegalArgumentException e) {
-      assertThat(e.getMessage(), is("Invalid `weight`: -1"));
+      assertThat(e.getMessage()).isEqualTo("Invalid `weight`: -1");
       throw e;
     }
   }
@@ -28,22 +27,22 @@ public class AccountRelationshipTest {
       AccountRelationship.fromWeight(4);
       fail();
     } catch (IllegalArgumentException e) {
-      assertThat(e.getMessage(), is("Invalid `weight`: 4"));
+      assertThat(e.getMessage()).isEqualTo("Invalid `weight`: 4");
       throw e;
     }
   }
 
   @Test
   public void fromWeight() {
-    assertThat(AccountRelationship.fromWeight(0), is(AccountRelationship.PARENT));
-    assertThat(AccountRelationship.fromWeight(1), is(AccountRelationship.PEER));
-    assertThat(AccountRelationship.fromWeight(2), is(AccountRelationship.CHILD));
+    assertThat(AccountRelationship.fromWeight(0)).isEqualTo(AccountRelationship.PARENT);
+    assertThat(AccountRelationship.fromWeight(1)).isEqualTo(AccountRelationship.PEER);
+    assertThat(AccountRelationship.fromWeight(2)).isEqualTo(AccountRelationship.CHILD);
   }
 
   @Test
   public void getWeight() {
-    assertThat(AccountRelationship.PARENT.getWeight(), is(0));
-    assertThat(AccountRelationship.PEER.getWeight(), is(1));
-    assertThat(AccountRelationship.CHILD.getWeight(), is(2));
+    assertThat(AccountRelationship.PARENT.getWeight()).isEqualTo(0);
+    assertThat(AccountRelationship.PEER.getWeight()).isEqualTo(1);
+    assertThat(AccountRelationship.CHILD.getWeight()).isEqualTo(2);
   }
 }
