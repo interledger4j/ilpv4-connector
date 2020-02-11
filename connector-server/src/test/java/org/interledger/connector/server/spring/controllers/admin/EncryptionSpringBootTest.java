dd2a203c5da5b6588126d11dcc466cc06f86c80e
nhartner <nhartner@gmail.com>
2020-02-11 11:17:57 -0700

    codacy cleanup
    Signed-off-by: nhartner <nhartner@gmail.com>

--- connector-server/src/test/java/org/interledger/connector/server/spring/controllers/admin/EncryptionSpringBootTest.java
+++ connector-server/src/test/java/org/interledger/connector/server/spring/controllers/admin/EncryptionSpringBootTest.java
@@ -21,11 +21,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -53,10 +51,6 @@ public class EncryptionSpringBootTest {
 
   @MockBean
   private AccountManager accountManagerMock;
-
-
-  @Autowired
-  private TestRestTemplate restTemplate;
 
   @Value("${interledger.connector.adminPassword}")
   private String adminPassword;
